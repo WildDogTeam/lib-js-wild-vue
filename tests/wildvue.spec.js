0af2953cf340aab54196869f3e11450be619249a
xiaoguangyu <xiaoguangyu@wilddog.com>
2016-05-17 12:01:12 +0800

    set timeout to 0

--- tests/wildvue.spec.js
+++ tests/wildvue.spec.js
@@ -9,6 +9,8 @@ var demoWilddogUrl = 'https://test123.wilddogio.com'
 
 describe('WildVue', function () {
   var wilddogRef
+  //network can be very slow
+  this.timeout(0);
 
   beforeEach(function (done) {
     wilddogRef = new Wilddog(demoWilddogUrl)
